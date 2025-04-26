be2868c6a0513db26cd06e4b7b623637785fcf50
rootdraws <rootdraws@gmail.com>
2025-04-26 10:45:04 -0500

    Added Events, and understand the timer.

--- contracts/PodcastNFT.sol
+++ contracts/PodcastNFT.sol
@@ -4,35 +4,51 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Limited Edition NFT which mints, and pays out to the artist.
-
-// Main NFT contract
+// Open edition NFT sale with a 24-hour time window
 contract PodcastNFT is ERC721URIStorage, Ownable {
-    // 1. Set Limited Edition Number
-    uint256 public constant MAX_SUPPLY = 20;
-    // 2. Set Price per NFT
-    uint256 public constant PRICE = 0.1 ether;
-    // 3. Tracks how many NFTs have been minted so far
+    // Price per NFT
+    uint256 public constant PRICE = 0.01 ether;
+    // Tracks how many NFTs have been minted so far
     uint256 public currentSupply = 0;
-    // 4. Set Address that receives mint payments
+    // Address that receives mint payments (burn address)
     address payable public artistAddress = payable(0x000000000000000000000000000000000000dEaD);
+    
+    // Time Variables
+    uint256 public saleStart;
+    uint256 public saleEnd; 
 
-    // 5. Constructor sets NFT name/symbol
+    // --- Events ---
+    event SaleStarted(uint256 saleStart, uint256 saleEnd);
+    event Minted(address indexed minter, uint256 indexed tokenId, string tokenURI);
+
+    // Constructor sets NFT name/symbol
     constructor()
         ERC721("PodcastNFT", "PNFT")
-        Ownable(msg.sender) // Sets deployer as owner
+        Ownable(msg.sender)
     {}
 
-    / / Mint a new NFT with a given metadata URI
+    // Start the sale: can only be called by the owner
+    function start() external onlyOwner {
+        require(saleStart == 0, "Sale already started");
+        saleStart = block.timestamp;
+        saleEnd = saleStart + 24 hours;
+        emit SaleStarted(saleStart, saleEnd);
+    }
+
+    // Mint a new NFT with a given metadata URI, only during the sale window
     function mint(string memory tokenURI) public payable {
-        require(currentSupply < MAX_SUPPLY, "Max supply reached");
+        require(saleStart != 0, "Sale not started");
+        require(block.timestamp >= saleStart, "Sale not started yet");
+        require(block.timestamp <= saleEnd, "Sale has ended");
         require(msg.value == PRICE, "Incorrect ETH amount sent");
 
         uint256 tokenId = currentSupply + 1;
-        _safeMint(msg.sender, tokenId);      // Mint NFT to sender
-        _setTokenURI(tokenId, tokenURI);     // Set metadata URI
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, tokenURI);
 
-        currentSupply++;                     // Increment supply
+        currentSupply++;
+
+        emit Minted(msg.sender, tokenId, tokenURI);
 
         // Send payment directly to artist
         (bool sent, ) = artistAddress.call{value: msg.value}("");
